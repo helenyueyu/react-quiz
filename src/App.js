8ca4ad081cbdef6b10339c74e3702d6a39483c10
tokyosuite <helenyueyu@gmail.com>
2019-01-14 16:04:31 -0800

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Divider, Button } from 'semantic-ui-react'
+
+const questions = ['What is 2 + 2?', 'What is 1 + 1?']
+const options = [[4, 5, 8, 10], [2, 3, 4, 5]]
+const correct = [4, 2]
+
+// Let's us know current question
+let counter = 0;
 
 class App extends Component {
+  state = {
+    question: questions[counter],
+    options: options[counter],
+    correctAnswer: correct[counter],
+    correct: 0
+  }
+  checkIfCorrect = (e) => {
+    if (correct[counter] === parseInt(e.target.innerHTML)) {
+      this.setState({
+        correct: this.state.correct + 1
+      })
+    }
+  }
+  handleClick = (e) => {
+    e.preventDefault()
+    this.checkIfCorrect(e)
+    counter++
+    this.setState({
+      question: questions[counter],
+      options: options[counter],
+      correctAnswer: correct[counter]
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container textAlign="center">
+      <Divider />
+      {
+        (counter === questions.length) ?
+        <h1>Thanks for playing! You have {this.state.correct}/{questions.length} correct!</h1> :
+        <React.Fragment><h2>React Quiz</h2>
+        <h3>{counter + 1}) {this.state.question}</h3>
+        {this.state.options.map(x => <li key={Math.random()}><Button primary onClick={this.handleClick}>{x}</Button></li>)}</React.Fragment>
+      }
+      </Container>
     );
   }
 }
